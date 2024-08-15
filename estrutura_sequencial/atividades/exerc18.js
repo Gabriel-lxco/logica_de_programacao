c38bffabe514ffef140864d36a66dd70cf70fda3
davy-alt <davy.nicacio@aluno.senai.br>
2024-08-15 10:07:53 -0300

    logia_de_programacao

--- estrutura_sequencial/atividades/exerc18.js
+++ estrutura_sequencial/atividades/exerc18.js
@@ -4,7 +4,7 @@ titulo = prompt("Digite o título de seu filme: ")
 minutos = parseFloat(prompt("Digite quanto tempo o seu filme possui em minutos: "))
 
 horas = minutos/60
-horas = (horas.toFixed(0))
+horas = Math.floor(horas)
 minutos = minutos - (horas * 60)
 
 document.write(`O filme ${titulo} possui ${horas}h ${minutos}m`)
