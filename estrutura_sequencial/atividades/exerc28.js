2dec31140d8e3339bf1da27bf39fd7a477be5945
SenagalZanfa24 <joao.g.oliveira12@aluno.senai.br>
2024-08-22 10:52:19 -0300

    logica_de_programacao

--- estrutura_sequencial/atividades/exerc28.js
+++ estrutura_sequencial/atividades/exerc28.js
@@ -1,12 +1,16 @@
-let hb, hf, fuso = 5, horas
+let hb, hf, fuso = 5
 
 hb = parseInt(prompt("Digite o horário atual do Brasil"))
 
 hf = hb + fuso
 
 if (hf > 23) {
+   if (hf > 28) {
+    document.write(`O horário informado está inválido!`)
+   } else {
     hf = hf - 24
     document.write(`De acordo com o horário atual do Brasil, o horário na França é igual à: ${hf}`)
+   }
 } else {
     document.write(`De acordo com o horário atual do Brasil, o horário na França é igual à: ${hf}`)
 }
