cc7e785d77e7d86a2832a462ecd75c2e2f1a25f9
SenagalZanfa24 <joao.g.oliveira12@aluno.senai.br>
2024-09-03 10:11:33 -0300

    Adicionando atividades

--- estrutura_sequencial/atividades/exerc32.js
+++ estrutura_sequencial/atividades/exerc32.js
@@ -1,5 +1,17 @@
 //
 
-while (condition) {
-    
+let contadora = 1
+let numero
+do {
+    numero = parseInt(prompt("Digite um número para ver os pares entre 1 e seu número:"))
+    if (isNaN(numero) || numero < 0) {
+        alert("Número inválido, digite um valor positivo e maior que 0")
+    }
+} while (isNaN(numero) || numero < 0);
+
+while (contadora != numero) {
+    if (contadora%2 == 0){
+        document.write(`${contadora} \t`)
+    }
+    contadora++
 }
