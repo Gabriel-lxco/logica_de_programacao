b8d74c85da64e5a4585f3d6993d7a921d238b3b8
SenagalZanfa24 <joao.g.oliveira12@aluno.senai.br>
2024-09-03 07:50:18 -0300

    Adicionando atividades

---
+++ estrutura_sequencial/atividades/exerc30.js
@@ -0,0 +1,6 @@
+// Construir uma tabuada com estrutura de 
+
+let numeros = Number(prompt("Digite um número para tabuada: "))
+for (let i = 1; i <= 10; i++){
+    document.write(`${numero} x ${i} = valor <br>`)
+}
