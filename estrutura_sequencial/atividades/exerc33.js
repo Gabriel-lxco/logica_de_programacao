9f82f489ae1c8e9222b2b6336ab2cb963c9d2347
João Gabriel <joao.g.nascimento17@aluno.senai.br>
2024-09-10 10:54:49 -0300

    Atualizando exercicio 33

---
+++ estrutura_sequencial/atividades/exerc33.js
@@ -0,0 +1,13 @@
+//
+let numero
+
+do {
+    numero = parseInt(prompt("Digite um número"))
+    if (numero <= 0 || isNaN(numero)) {
+        alert("Digite um número válido")
+    }
+} while (numero <= 0 || isNaN(numero));
+
+if () {
+    
+}
